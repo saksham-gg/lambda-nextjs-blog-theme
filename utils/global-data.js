6986888ee8d258b2e8546aab1a07fc717148601f
Saksham Garg <106537109+saksham-gg@users.noreply.github.com>
2024-03-27 16:11:38 +0530

    days

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme changed to test again please!';
+    : 'Next.js Blog Theme changed after days!';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
