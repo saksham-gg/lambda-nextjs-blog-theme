7f46cc1cdd1760cb77f2fa10d969cd90883997ff
Saksham Garg <106537109+saksham-gg@users.noreply.github.com>
2024-04-01 13:28:44 +0530

    longg

--- utils/global-data.js
+++ utils/global-data.js
@@ -2,9 +2,9 @@ export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
     : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
+  const blogTitle = process.env.BLOG_TITLEs
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme changed latesttt for-net build 2!';
+    : 'Next.js Blog Theme changed lomnggng!';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
