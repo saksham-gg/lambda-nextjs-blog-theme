319de6d60e0f9268ea0f20850d84e52c7e384e53
Saksham Garg <106537109+saksham-gg@users.noreply.github.com>
2024-04-02 17:01:20 +0530

    vercel

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLEs
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme changed lomnggng!';
+    : 'Next.js Blog Theme changed vercelll!';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
